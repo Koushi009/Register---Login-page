82c12bfe1f83fc55e9193d50b06a7927fc439aa6
Shid0x <thomas.leguilloudp@gmail.com>
2018-04-29 13:12:16 +0200

    random salt generator
    The script now work, it generates a random salt per user along with a pass encrypted in sha256
    still needing to review if it works with different source code.

--- Index.php
+++ Index.php
@@ -13,6 +13,8 @@ if (isset($_POST['username']) && isset($_POST['password'])){
 	$username= $_POST['username'] ;
 	$passtemp= $_POST['password'] ;
 	$password = hash('sha256',$passtemp);
+	$salt = uniqid(mt_rand(), true);
+
 	// $salt = hash('sha256',$passtemp); - Ok so that was dumb
 	
 	 // $query = "INSERT INTO 'accounts' (username,password,salt) VALUES ('$username','$password','$salt')" ;
